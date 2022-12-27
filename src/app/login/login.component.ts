7aa45cce62900e2fc525414c86347ad6b68f2ff0
Kovács Dorina <kovacsdorina2@gmail.com>
2022-12-27 11:22:49 +0100

    login component kész

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  loginForm !: FormGroup;
+  constructor( private auth: AuthService, private formBuilder: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      user: [''],
+      password: [''],
+
+    })
+  }
+
+  signIn(){
+    let user = this.loginForm.value.email;
+    let pass = this.loginForm.value.password;
+    
+    this.auth.signIn(user, pass).subscribe(res => {
+      localStorage.setItem("token", res.token);
+      localStorage.setItem("email", res.email);
+    })
+    console.error("sikerelen");
+    
+  }
 }
