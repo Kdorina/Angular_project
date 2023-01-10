6a0d3dd33602eeb6df27f8eba86a97e033ba76e0
Kovács Dorina <kovacsdorina2@gmail.com>
2023-01-10 17:56:34 +0100

    tantárgyak szerkeztése törlése felvétele token miatt még nem lehetséges (de működne)

--- src/app/subjects/subjects.component.ts
+++ src/app/subjects/subjects.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 import { ApiService } from '../service/api.service';
 
 @Component({
@@ -8,14 +9,14 @@ import { ApiService } from '../service/api.service';
   styleUrls: ['./subjects.component.scss']
 })
 export class SubjectsComponent implements OnInit{
-  constructor(private api:ApiService, ){}
+  constructor(private api:ApiService ){}
 
   data: any;
-  
-ngOnInit(): void {
- this.index()
-}
 
+ngOnInit(): void {
+ this.index();
+
+}
 
 index(){
    this.data= this.api.index().subscribe((res)=>{
@@ -23,6 +24,42 @@ index(){
     console.log(this.data)
   })
 }
+
+//tantárgy hozzáadás és eltárolása
+
+subjects: any;
+
+store(newSubject:string){
+this.subjects ={
+  'subject':newSubject,
+};
+
+this.api.store().subscribe(res=>{
+  this.subjects = res
+});
+console.log(this.subjects);
+}
+
+
+//tantárgy szerkeztése
+
+subjectId: any;
+editSubject: any;
+
+update(clickSubject:string){
+
+  this.api.update(this.subjectId).subscribe((res)=>{
+    console.log("Sikeres szerkeztés!");
+  })
+
+}
+show: any;
+editShow(){
+  this.show = true;
+}
+
+//tantárgy törlése
+
 destroy(id:any){
   this.api.destroy(id).subscribe(res=>{
 
