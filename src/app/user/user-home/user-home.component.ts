f4b64c57a2e88c70c8c3e35550e049a77aa7af37
Kovács Dorina <kovacsdorina2@gmail.com>
2023-01-20 00:18:06 +0100

    user home oldal formázás

--- src/app/user/user-home/user-home.component.ts
+++ src/app/user/user-home/user-home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-home',
   templateUrl: './user-home.component.html',
   styleUrls: ['./user-home.component.scss']
 })
-export class UserHomeComponent {
+export class UserHomeComponent implements OnInit{
 
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+  
+  }
+
+  // navigateSub(){
+  //   this.router.navigate(['/user/subject'])
+  // }
+  // navigateStat(){
+  //   this.router.navigate(['/user/statist'])
+  // }
+  // navigateFile(){
+  //   this.router.navigate(['/user/file'])
+  // }
 }
