b57ae84bd568e6a1e7b12bee4dd734f002b4e004
Kovács Dorina <kovacsdorina2@gmail.com>
2023-01-20 08:35:16 +0100

    admin guard megirasa / működik

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -6,31 +6,36 @@ import { AdminSchoolsComponent } from "./admin-schools/admin-schools.component";
 import { AdminStatisticComponent } from "./admin-statistic/admin-statistic.component";
 import { AdminStudentsComponent } from "./admin-students/admin-students.component";
 import { AdminComponent } from "./admin.component";
+import { AdminGuard } from "./service/admin.guard";
 
 const routes: Routes = [
   {path: "admin", component: AdminComponent,
   children: [{
-      path:"login", component:AdminLoginComponent
+      path:"login", component:AdminLoginComponent,
   }]},
 
     {path: "admin", component: AdminComponent,
   children: [{
-      path:'home', component: AdminHomeComponent
+      path:'home', component: AdminHomeComponent,
+      canActivate:[AdminGuard]
   }]},
 
     {path: "admin", component: AdminComponent,
     children: [{ 
-      path:"students", component: AdminStudentsComponent
+      path:"students", component: AdminStudentsComponent,
+      canActivate:[AdminGuard]
   }]},
 
     {path: "admin", component: AdminComponent,
     children: [{ 
-      path:"schools", component: AdminSchoolsComponent
+      path:"schools", component: AdminSchoolsComponent,
+      canActivate:[AdminGuard]
   }]},
   
     {path: "admin", component: AdminComponent,
     children: [{ 
-      path:"statistic", component: AdminStatisticComponent
+      path:"statistic", component: AdminStatisticComponent,
+      canActivate:[AdminGuard]
   }]},
 ]
   
