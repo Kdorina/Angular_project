35886c15133086d33907c41bb2becbb19ca5e543
Kdorina <kovacsdorina2@gmail.com>
2023-04-24 22:33:40 +0200

    ha a felhasználó be van jelentkezve , jelenjen meg neve a navbaron -> működik (formázást igényel még)

--- src/app/user/user-navbar/user-navbar.component.ts
+++ src/app/user/user-navbar/user-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
 
@@ -7,10 +7,23 @@ import { AuthService } from 'src/app/service/auth.service';
   templateUrl: './user-navbar.component.html',
   styleUrls: ['./user-navbar.component.scss']
 })
-export class UserNavbarComponent {
+export class UserNavbarComponent implements OnInit{
 
   constructor(private auth: AuthService, private router: Router){}
+
+  ngOnInit(): void {
+    this.isLoggedIn();
+  }
   userNavbar = false;
+  loggedIn:any;
+  currentU:any;
+  isLoggedIn(){
+    let jsonCurrentUser: any = localStorage.getItem("currentUser");
+    let currentUser = JSON.parse(jsonCurrentUser);
+    console.log(currentUser.name);
+    this.currentU = currentUser.name
+    this.loggedIn = true;
+  }
 
   user(){
     this.userNavbar = !this.userNavbar;
@@ -19,7 +32,6 @@ export class UserNavbarComponent {
     let jsonCurrentUser: any = localStorage.getItem("currentUser");
     let currentUser = JSON.parse(jsonCurrentUser);
     console.log(currentUser.name);
-    console.log(currentUser.token);
     this.auth.logout(currentUser.email, currentUser.token).subscribe({
       next: res => {
         console.log(res);
@@ -27,6 +39,6 @@ export class UserNavbarComponent {
         this.router.navigate(['home'])
       }
     });
-      
+
   }
 }
